6a6a383cf72b35ca8c30b2b4fd33d59e73ac39d5
drfiveminusmint <drfiveminusminusplays@gmail.com>
2023-07-16 14:51:07 -0400

    another test, last one for today probably

--- modules/Movecraft/src/main/java/net/countercraft/movecraft/async/AsyncManager.java
+++ modules/Movecraft/src/main/java/net/countercraft/movecraft/async/AsyncManager.java
@@ -273,7 +273,7 @@ public class AsyncManager extends BukkitRunnable {
                 if (!entity.getType().equals(EntityType.PRIMED_TNT))
                     continue;
                 Bukkit.getServer().broadcastMessage("Found TNT with fuse of:" + ((TNTPrimed)entity).getFuseTicks());
-                if (((TNTPrimed)entity).getFuseTicks() <= 5) { //better safe than sorry
+                if (((TNTPrimed)entity).getFuseTicks() <= 20) { //better safe than sorry
                     skipTick = true;
                     break;
                 }
